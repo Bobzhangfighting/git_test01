2e599a15831982682cf00f27c14aea0954ba1dab
Bobzhangfighting <15200706014@163.com>
2016-05-18 19:41:06 +0800

    git test01
    Signed-off-by: Bobzhangfighting <15200706014@163.com>

---
+++ git01/src/main/java/com/yc/git01/App.java
@@ -0,0 +1,14 @@
+package com.yc.git01;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("test of git 01");
+    }
+}
